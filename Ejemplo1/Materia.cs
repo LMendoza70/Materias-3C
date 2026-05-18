ec9ce060f2d3b63752a732bc043e125042cfc6bc
MTI.LuisAlbertoMendoza <luis.mendoza@uthh.edu.mx>
2026-05-18 11:29:05 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo1/Materia.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materias.Ejemplo1
+{
+    public class Materia
+    {
+
+        //Ambito de variables 
+        //public -> lo puede ver cualquiera 
+        //rotected -> explicacion pendiente para cuando toquemos herencia 
+        //private-> solo se puede utilizar dentro de la clase contenedora 
+        //definimos los atributos(variables) que vamos a utilizar
+        private float parcial1;
+        private float parcial2;
+        private float parcial3;
+        private string nombreMateria;
+
+        //definimos las propiedades de nuestro ejemplo
+        public float Parcial1
+        {
+            get { return parcial1; }
+            set { if (value >= 0 && value <= 10)
+                {
+                    parcial1= value;
+                }
+                else
+                {
+                    parcial1 = -1;
+                }
+            }
+        }
+
+        public float Parcial2
+        {
+            get { return parcial2; }
+            set
+            {
+                if(value>=0 && value <= 10)
+                {
+                    parcial2=value;
+                }
+                else
+                {
+                    parcial2 = -1;
+                }
+            }
+        }
+
+        //definimos el/los metodos  utilizar en nuestro ejercicio
+        public float CalculaPromedio()
+        {
+            float resultado = 0f;
+            resultado = (parcial1 + parcial2 + parcial3) / 3;
+            return resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Materias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
