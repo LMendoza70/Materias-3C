0b6332c24a96e722734981f82dca2318e3f4ea8d
MTI.LuisAlbertoMendoza <luis.mendoza@uthh.edu.mx>
2026-05-25 12:18:15 -0600

    Se implemento herencia y polimorfismo

---
+++ Cafeteria/BebidaCaliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materias.Cafeteria
+{
+    internal class BebidaCaliente:Bebidas
+    {
+        //atributos
+        private float Temperatura;
+
+        //propiedades
+        public float tempetarura
+        {
+            get { return Temperatura; }
+            set { Temperatura = value; }
+        }
+
+        public BebidaCaliente(string _nombre,string _tamaño, float _precio, float _temperatura):base(_nombre,_tamaño,_precio)
+        {
+            Temperatura= _temperatura;
+        }
+
+        public override string Preparar()
+        {
+            return "Preparando un : "+Nombre+ " de  tamaño : "+Tamaño+" con temperatura "+Temperatura;
+        }
+    }
+}
---
+++ Cafeteria/Bebidas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materias.Cafeteria
+{
+    internal class Bebidas
+    {
+        protected string Nombre;
+        protected float Precio;
+        protected string Tamaño;
+
+        public string nombre
+        {
+            get {  return Nombre; }
+            set { Nombre = value; }
+        }
+
+        public string tamaño
+        {
+            get { return Tamaño; }
+            set { Tamaño = value; }
+        }
+
+        public float precio
+        {
+            get { return Precio; }
+            set
+            {
+                if (Precio > 0)
+                {
+                    Precio = value;
+                }
+                else
+                {
+                    Precio = -1;
+                }
+            }
+        }
+
+        //Constructor
+        public Bebidas()
+        {
+            Precio = 0;
+            Nombre = "";
+            Tamaño = "";
+        }
+        //contructor parametrizado
+        public Bebidas(string _nombre, string _tamaño, float _precio)
+        {
+            this.Nombre = _nombre;
+            Precio=_precio;
+            Tamaño = _tamaño;
+        }
+        //////////////
+        //metodo preparar
+        public virtual string Preparar()
+        {
+            return "Estamos preparando un : "+Nombre +" de tamaño "+Tamaño;
+        }
+
+        public void Descuento(float descuento)
+        {
+            Precio = Precio * (1 - (descuento / 100));
+        }
+    }
+}
